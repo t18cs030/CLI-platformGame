deb91b8388b375548a53658ee6ea681b594de3fe
csuser <csuser@vbox-peppermint>
2021-05-28 12:24:06 +0900

    run()関数を仮実装した

--- src/Model.java
+++ src/Model.java
@@ -1,6 +1,14 @@
 
 public class Model {
+	
+	private void run() {
+		// TODO 自動生成されたメソッド・スタブ
+		return;
+	}
+	
 	public static void main(String[] args) {
 		Model model = new Model();
+		model.run();
 	}
+
 }
