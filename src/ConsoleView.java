5fd3f8383d4444d2ee0e057aefac653a4a952b0b
csuser <csuser@vbox-peppermint>
2021-05-31 11:05:10 +0900

    Viewクラスに初期画面を設定した

--- src/ConsoleView.java
+++ src/ConsoleView.java
@@ -1,5 +1,9 @@
 
 public class ConsoleView {
+	
+	private final static int WIDTH = 80;
+	private final static int HEIGHT = 24;
+	private char screan[][];
 	private Model model;
 	private int width;
 	private int height;
@@ -8,6 +12,16 @@ public class ConsoleView {
 		this.model = model;
 		this.width = width;
 		this.height = height;
+		this.screan = new char[width][height];
+		clear();
+	}
+	
+	public void clear() {
+		for(int y=0; y<height;y++) {
+			for(int x=0;x<width;x++) {
+				screan[x][y] = '-';
+			}
+		}
 	}
 
 }
