04d792d19d5f4beb3a1ea1fa3ef3c9c6fa5dc345
csuser <csuser@vbox-peppermint>
2021-05-31 11:55:47 +0900

    自機の表示をするためにコンストラクタとPaint関数を作成

--- src/Pleyer.java
+++ src/Pleyer.java
@@ -1,8 +1,18 @@
 
 public class Pleyer {
+	
+	private  char c;
+	private int x;
+	private int y;
 
 	public Pleyer() {
 		// TODO 自動生成されたコンストラクター・スタブ
+		this.c='@';
+		this.x=20;
+		this.y=10;
+	}
+	public void paint(ConsoleView view) {
+		view.put(c,x,y);
 	}
 
 }
