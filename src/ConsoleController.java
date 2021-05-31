9a24590209e98ec7b3d1882e8fcf222b2f505946
csuser <csuser@vbox-peppermint>
2021-05-31 11:35:07 +0900

    Controller関数にTimerの実施

--- src/ConsoleController.java
+++ src/ConsoleController.java
@@ -1,13 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ConsoleController {
+import javax.swing.Timer;
 
-	public ConsoleController(Model model) {
+public class ConsoleController implements ActionListener {
+	
+	private final static int DELAY = 50;
+	private Model model;
+	private Timer timer;
+
+	public ConsoleController(Model model){
 		// TODO 自動生成されたコンストラクター・スタブ
+		this.model = model;
+		timer = new Timer(DELAY,this);
 		
 	}
 
 	public void run() {
 		// TODO 自動生成されたメソッド・スタブ
+		// キー入力の処理
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 時間経過の処理
 		
 	}
 
